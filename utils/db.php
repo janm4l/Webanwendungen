228c881fd89af96f01bb3c771dbb10b6e183646f
Jan Malte Senger <jmsenger@web.de>
2023-08-16 09:48:18 +0200

    Erste Erstellung von Dateien

---
+++ utils/db.php
@@ -0,0 +1,8 @@
+<?
+$conn = mysqli_connect("provadis.jmsenger.de", "provadis", "AmirAmirAmir123");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+?>
