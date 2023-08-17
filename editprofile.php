16303a9c2053b95bdd62b5cba0bafbe46f0b56d6
Jonas <j.lammerding@gmx.de>
2023-08-17 17:03:51 +0200

    sprache auf englisch vereinheitlicht

--- editprofile.php
+++ editprofile.php
@@ -23,8 +23,8 @@ $info = getUserInfo();
     <input type="text" id="forename" name="forename" placeholder="Max" value="<?php echo $info['forename'] ?>">
     <input type="text" id="name" name="name" placeholder="Mustermann" value="<?php echo $info['name'] ?>">
     <input type="text" id="username" name="username" placeholder="mein nutzername" value="<?php echo $info['username'] ?>">
-    <input type="text" id="street" name="street" placeholder="meine Strasse" value="<?php echo $info['street'] ?>">
-    <input type="text" id="street_number" name="street_number" placeholder="meine Strasse" value="<?php echo $info['street_number'] ?>">
+    <input type="text" id="street" name="street" placeholder="meine Street" value="<?php echo $info['street'] ?>">
+    <input type="text" id="street_number" name="street_number" placeholder="meine Hausnummer" value="<?php echo $info['street_number'] ?>">
     <input type="text" id="postcode" name="postcode" placeholder="44444" value="<?php echo $info['postcode'] ?>">
     <input type="text" id="city" name="city" placeholder="meine Stadt" value="<?php echo $info['city'] ?>">
     <input type="text" id="create_datetime" name="create_datetime" placeholder="account erstellt am: 01.01.2000" value="<?php echo $info['create_datetime'] ?>">
--- profile.php
+++ profile.php
@@ -38,7 +38,7 @@ if($_SERVER["REQUEST_METHOD"] === 'POST'){
     <input type="text" id="name" name="name" placeholder="Mustermann" readonly="readonly" value="<?php echo $info['name'] ?>">
     <input type="text" id="username" name="username" placeholder="mein username" readonly="readonly" value="<?php echo $info['username'] ?>">
     <input type="text" id="street" name="street" placeholder="meine street" readonly="readonly" value="<?php echo $info['street'] ?>">
-    <input type="text" id="street_number" name="street_number" placeholder="meine street" readonly="readonly" value="<?php echo $info['street_number'] ?>">
+    <input type="text" id="street_number" name="street_number" placeholder="meine Hausnummer" readonly="readonly" value="<?php echo $info['street_number'] ?>">
     <input type="text" id="postcode" name="postcode" placeholder="44444" readonly="readonly" value="<?php echo $info['postcode'] ?>">
     <input type="text" id="city" name="city" placeholder="meine city" readonly="readonly" value="<?php echo $info['city'] ?>">
     <input type="text" id="accountErstelltAm" name="accountErstelltAm" placeholder="account erstellt am: 01.01.2000" readonly="readonly" value="<?php echo $info['create_datetime'] ?>">
